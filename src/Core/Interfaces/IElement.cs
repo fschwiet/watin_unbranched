f8ea15ab8b1269c030a0587856c64451adfb1eaf
Edward Wilde <ewilde505@unknownserver.com>
2007-11-13 11:29:54 +0000

    Implemented NextSibling for FF. Note that the test NextSibling fails for IE for the following reason:
    The implementation of  IE.NextSibling assumes (by casting to IHTMLElement) that the next DOM node will be an htmlelement. From a brief investigation this seems to be mostly a correct assumption, however in some cases (such as in this test) the NextSibling will not be an HTMLElement but perhaps a text node. One way to fix this would be to cast as IHTMLDomNode and check the NodeType.
    
    For the FF implementation of NextSibling if the next node is not an HTMLElement I grab the NextSibling until we encounter a node which is an HTMLElement, making the function really NextHtmlElementSibling.
    
    Need to decide what is correct behaviour and update both implementations if required, (could be the IE implementation is the way we want to go anyway).

--- src/Core/Interfaces/IElement.cs
+++ src/Core/Interfaces/IElement.cs
@@ -20,6 +20,8 @@ namespace WatiN.Core.Interfaces
 {
     public interface IElement
     {
+        #region Properties
+
         /// <summary>
         /// Gets the id.
         /// </summary>
@@ -38,5 +40,71 @@ namespace WatiN.Core.Interfaces
         /// <param name="attributeName">The attribute name</param>
         /// <returns></returns>
         string GetAttributeValue(string attributeName);
+        /// <summary>
+        /// Gets the innertext of this element (and the innertext of all the elements contained
+        /// in this element).
+        /// </summary>
+        /// <value>The inner text of this element</value>
+        string Text { get; }
+
+        /// <summary>
+        /// Gets the title of the element.
+        /// </summary>
+        /// <value>The title of this element.</value>
+        string Title { get; }
+
+        /// <summary>
+        /// Gets the inner HTML of the element.
+        /// </summary>
+        /// <value>The inner HTML of the element.</value>
+        string InnerHtml { get; }
+
+        /// <summary>
+        /// Gets the parent element of this element.
+        /// If the parent type is known you can cast it to that type.
+        /// </summary>
+        /// <value>The parent.</value>
+        /// <example>
+        /// The following example shows you how to use the parent property.
+        /// Assume your web page contains a bit of html that looks something
+        /// like this:
+        /// 
+        /// div
+        ///   a id="watinlink" href="http://watin.sourceforge.net" /
+        ///   a href="http://sourceforge.net/projects/watin" /
+        /// /div
+        /// div
+        ///   a id="watinfixturelink" href="http://watinfixture.sourceforge.net" /
+        ///   a href="http://sourceforge.net/projects/watinfixture" /
+        /// /div
+        /// Now you want to click on the second link of the watin project. Using the 
+        /// parent property the code to do this would be:
+        /// 
+        /// <code>
+        /// Div watinDiv = (Div) ie.Link("watinlink").Parent;
+        /// watinDiv.Links[1].Click();
+        /// </code>
+        /// </example>
+        IElement Parent { get; }
+
+        /// <summary>
+        /// Gets the next sibling of this element in the Dom of the HTML page.
+        /// </summary>
+        /// <value>The next sibling.</value>		
+        IElement NextSibling { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Clicks this element and waits till the event is completely finished (page is loaded 
+        /// and ready) .
+        /// </summary>
+        void Click();
+
+        #endregion
+
+
     }
 }
