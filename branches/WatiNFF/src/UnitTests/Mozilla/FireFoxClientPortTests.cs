69937217fbbc77655c3da940e33a76dbe4b87f64
Edward Wilde <ewilde505@unknownserver.com>
2008-01-20 13:49:54 +0000

    temp checkin moving pc
    Added unit test for FF restore dialog

--- branches/WatiNFF/src/UnitTests/Mozilla/FireFoxClientPortTests.cs
+++ branches/WatiNFF/src/UnitTests/Mozilla/FireFoxClientPortTests.cs
@@ -16,6 +16,7 @@
 
 #endregion Copyright
 
+using System.Diagnostics;
 using System.IO;
 using NUnit.Framework;
 using WatiN.Core.Logging;
@@ -24,7 +25,7 @@ using WatiN.Core.Mozilla;
 namespace WatiN.Core.UnitTests.Mozilla
 {
     [TestFixture]
-    public class FireFoxClientPortTests
+    public class FireFoxClientPortTests : WatiNTest
     {
         [TestFixtureSetUp]
         public void Setup()
@@ -59,6 +60,23 @@ namespace WatiN.Core.UnitTests.Mozilla
         }
 
         /// <summary>
+        /// Test that an error is thrown if you try to connect with an instance of FireFox already open
+        /// </summary>
+        [Test, ExpectedException(typeof(FireFoxException))] 
+        public void ShouldNotConnectWithRunningInstances()
+        {
+
+            using (FireFoxClientPort ffPort = new FireFoxClientPort())
+            {
+                using (Process existingInstance = new Process())
+                {
+                    existingInstance.StartInfo.FileName = ffPort.PathToExe;
+                    existingInstance.Start();
+                    ffPort.Connect();
+                }
+            } 
+        }
+        /// <summary>
         /// Test that we can retrieve the path to the FireFox executable using the registry
         /// </summary>
         [Test]
@@ -69,6 +87,84 @@ namespace WatiN.Core.UnitTests.Mozilla
                 Assert.IsNotNull(ffPort.PathToExe, "Did not find the path to the FireFox executable");
                 Assert.IsTrue(ffPort.PathToExe.Contains("firefox.exe"), "Did not find the expected value for the path to the FireFox executable");
                 Assert.IsTrue(File.Exists(ffPort.PathToExe), string.Format("{0} does not exist.", ffPort.PathToExe));
+            }
+        }
+
+        /// <summary>
+        /// This test tries to stress the communication between WaitN and the ssh server to ensure that our networking code
+        /// is stable
+        /// </summary>
+        [Test]
+        public void Stress()
+        {
+            using (FireFoxClientPort ffPort = new FireFoxClientPort())
+            {
+                ffPort.Connect();
+                ffPort.Write("{0}.loadURI(\"{1}\")", FireFoxClientPort.BrowserVariableName, MainURI);
+                ffPort.Write("for (i = 0; i < 1000; i++){links = doc.getElementsByTagName(\"a\");links.length;} true;");
+                Assert.IsTrue(ffPort.LastResponseAsBool);
+
+                for (int i =0 ; i < 50; i++)
+                {
+                    ffPort.Write("{0}.loadURI(\"{1}\")", FireFoxClientPort.BrowserVariableName, MainURI);                
+                    ffPort.Write("for (i = 0; i < 1000; i++){links = doc.getElementsByTagName(\"a\");links.length;} true;");
+                    Assert.IsTrue(ffPort.LastResponseAsBool);
+                }
+            }
+        }
+
+        /// <summary>
+        /// If FireFox does not shut down correctly from a previous session you may get
+        /// a dialog appear with the window text "Firefox - Restore Previous Session".
+        /// This test simulates a bad Firefox shutdown and checks that our connect method
+        /// can deal with this dialog when shelling a new instance of Firefox.
+        /// </summary>
+        [Test]
+        public void HandleStartNewSessionDialog()
+        {
+            using (Process process = new Process())
+            {
+                using (FireFoxClientPort clientPort = new FireFoxClientPort())
+                {
+                    // Simulate a bad shut down.
+                    process.StartInfo.FileName = clientPort.PathToExe;
+                    process.Start();
+                    process.WaitForInputIdle(5000);
+                    process.Kill();
+
+                    // Now try and connect
+                    clientPort.Connect();
+
+                    Assert.IsTrue(clientPort.Connected, "FireFoxClientPort.Connect did not handle the restore previous dialig scenario.");
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Test that an exception is raised if an attempt to write is 
+        /// made before connecting successfully to the jssh server.
+        /// </summary>
+        [Test, ExpectedException(typeof(FireFoxException))]
+        public void WriteBeforeConnect()
+        {
+            using (FireFoxClientPort ffPort = new FireFoxClientPort())
+            {
+                ffPort.Write("for (i = 0; i < 1000; i++){links = doc.getElementsByTagName(\"a\");links.length;} return true;");
+                Assert.IsTrue(ffPort.LastResponseAsBool);
+            }
+        }
+
+        /// <summary>
+        /// Tests the behaviour of the <see cref="FireFoxClientPort.IsMainWindowVisible"/> property.
+        /// </summary>
+        [Test]
+        public void IsMainWindowVisible()
+        {
+            using (FireFoxClientPort ffPort = new FireFoxClientPort())
+            {
+                ffPort.Connect();
+                Assert.IsTrue(ffPort.IsMainWindowVisible);
             }
         }
     }
